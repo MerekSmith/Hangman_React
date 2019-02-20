0155ee9f7aaf036986780f2ff48eea7fb612a8b1
Merek Smith <mesmith@overstock.com>
2019-02-20 15:53:43 -0700

    adding random word functions

--- src/components/game.js
+++ src/components/game.js
@@ -8,34 +8,74 @@ export default class Game extends Component {
     super(props);
     this.state = {
       letter: "",
-      phone: "",
-      senderEmail: "",
-      message: "",
-      mailSent: false,
-      error: null
+      guesses: 10,
+      wins: 0,
+      losses: 0,
+      word: "",
+      wordArray: [],
+      guessedLetters: []
     };
+
+
+
+
+
+
+
+    // end of constructor
   }
+
+
 
   componentWillMount() {
     window.addEventListener('keyup', this.keyboardInput);
+    // this.chooseWord();
   }
 
   componentWillUnmount() {
     window.removeEventListener('keyup', this.keyboardInput);
   }
 
+  // componentDidMount() {
+  //   this.chooseWord();
+  // }
 
   keyboardInput = event => {
-    console.log('event', event);
-    console.log('event key', event.key);
+    // console.log('event', event);
+    console.log('event key', event.key.toUpperCase());
+    this.setState({
+      letter: event.key.toUpperCase()
+    })
   };
+
+  // Initialize random word. 
+  chooseWord = () => {
+    const hangmanWords = ['Disney', 'Ariel', 'Belle', 'Moana', 'Elsa', 'Anna', 'Castle', 'Princess', 'Prince', 'Mulan', 'Aurora', 'Tiana', 'Snow White', 'Cinderella', 'Repunzel', 'Princess and the Frog', 'Beauty and the Beast', 'Brave', 'Merida', 'Sleeping Beauty', 'Frozen', 'Charming', 'Jasmine', 'Aladdin', 'Tangled'];
+
+    let word = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
+    let wordArray = word.split('');
+    console.log(wordArray);
+
+    this.setState({
+      word,
+      wordArray
+    }, console.log('1 after setstate', this.state.word));
+    console.log('2 state', this.state.word);
+    console.log('3 word', word);
+  }
+
+
 
 
   render() {
     return (
       <div className="game-container">;
-    <h1>Hangman!</h1>
-        <input type="name" onKeyPress={this.handleKeyPress} />
+        <h1>Hangman!</h1>
+        <button onClick={this.chooseWord}>start</button>
+        <h1>{this.state.letter}</h1>
+        <h1>{this.state.word}</h1>
+        <h1>{this.state.wordArray}</h1>
+
       </div>
     )
   }

99d27cc905eefa182d2aaef49f64da92dbf4735d
Merek Smith <mesmith@overstock.com>
2019-02-25 14:14:35 -0700

    working game

--- src/components/game.js
+++ src/components/game.js
@@ -13,11 +13,14 @@ export default class Game extends Component {
       losses: 0,
       word: "",
       answerArray: [],
-			guessedLetters: [],
-			answerWord: "",
-			badKey: null,
-			dupKey: null,
-			playing: false
+      guessedLetters: [],
+      answerWord: [],
+      badKey: null,
+      dupKey: null,
+      started: false,
+      playing: false,
+      win: null,
+      lose: null
     };
 
     // end of constructor
@@ -39,114 +42,154 @@ export default class Game extends Component {
   // }
 
   keyboardInput = event => {
-		if(!this.state.playing) {
-			return false;
-		}
+    if (!this.state.playing) {
+      return false;
+    }
 
-		let key = event.key.toUpperCase();
-		// let badKey = null;
-		const letterChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-		
-		let answerArray = this.state.answerArray;
-		let answerWord = this.state.answerWord.split('');
-		let guesses = this.state.guesses;
-		let guessedLetters = this.state.guessedLetters;
-		let wins = this.state.wins;
-		let losses = this.state.losses;
+    let key = event.key.toUpperCase();
+    // let badKey = null;
+    const letterChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    let answerArray = this.state.answerArray;
+    let answerWord = this.state.answerWord;
+    let guesses = this.state.guesses;
+    let guessedLetters = this.state.guessedLetters;
+    let wins = this.state.wins;
+    let losses = this.state.losses;
 
 
-		// Check if key is a letter.
-		if(!letterChoices.includes(key)) {
-			this.setState({
-				badKey: true
-			});
-			console.log('badKey has been set')
-			return false;
-		};
-		
-		// Check if key has already been guessed.
-		if(guessedLetters.includes(key) || answerWord.includes(key)) {
-			this.setState({
-				dupKey: true
-			});
-			console.log('dupKey has been set')
-			return false;
-		}
+    // Check if key is a letter.
+    if (!letterChoices.includes(key)) {
+      this.setState({
+        badKey: true
+      });
+      return false;
+    };
 
-		// Check if letter is in the word.
-		if(answerArray.includes(key)) {
-			for (let i = 0; i < answerArray.length; i++) {
-				console.log('answerArray', answerArray);
-				console.log('key match', key, answerArray[i], key === answerArray[i]);
-				if(key === answerArray[i]) {
-					answerWord[i] = key;
-				} 
-			}
-			if(!answerWord.includes("_")) {
-				wins += 1;
-				this.chooseWord();
-				return false;
-			}
-		} else {
-			guesses -= 1;
-			if(guesses === 0) {
-				// put lose message function here.
-				losses += 1;
-				this.chooseWord();
-				return false;
-			}
-			guessedLetters.push(key);
-		}
+    // Check if key has already been guessed.
+    if (guessedLetters.includes(key) || answerWord.includes(key)) {
+      this.setState({
+        dupKey: true
+      });
+      return false;
+    }
 
-		answerWord = answerWord.join('');
-		console.log(guessedLetters);
+    // Check if letter is in the word.
+    if (answerArray.includes(key)) {
+      for (let i = 0; i < answerArray.length; i++) {
+        if (key === answerArray[i]) {
+          answerWord[i] = key;
+        }
+      }
+      // answerWord = answerWord.join('');
+      if (!answerWord.includes("_")) {
+        wins += 1;
+        this.setState({
+          wins,
+          win: true,
+          answerWord,
+          playing: false
+        });
+        setTimeout(this.end(), 5000);
+        return false;
+      }
+    } else {
+      guesses -= 1;
+      this.setState({
+        guesses
+      });
+      if (guesses === 0) {
+        // put lose message function here.
+        losses += 1;
+        this.setState({
+          losses,
+          lose: true,
+          answerWord,
+          playing: false
+        });
+        setTimeout(this.end(), 5000);
+        return false;
+      }
+      guessedLetters.push(key);
+    }
+
+    
 
     this.setState({
-			letter: key,
-			badKey: null,
-			dupKey: null,
-			answerWord,
-			guesses,
-			guessedLetters,
-			losses,
-			wins
+      letter: key,
+      badKey: null,
+      dupKey: null,
+      answerWord,
+      guessedLetters
     })
+  };
+
+
+  // end = () => {
+  //   console.log('win hit', this.state.win);
+
+  //   this.setState({
+  //     win: null,
+  //     lose: null
+  //   });
+
+  //   this.chooseWord();
+
+
+  //   console.log('win hit 2', this.state.win);
+
+  // };
+
+
+  end = () => {
+    console.log('lose hit', this.state.win);
+    setTimeout(() => {
+      this.setState({
+        win: null,
+        lose: null
+      });
+      this.chooseWord();
+    },3000);
+
+
+    console.log('lose hit 2', this.state.win);
+
   };
 
 
 
   // Initialize random word. 
   chooseWord = () => {
-		this.setState({
-			playing: true
-		});
+    this.setState({
+      started: true,
+      playing: true
+    });
 
     const hangmanWords = ['Disney', 'Ariel', 'Belle', 'Moana', 'Elsa', 'Anna', 'Castle', 'Princess', 'Prince', 'Mulan', 'Aurora', 'Tiana', 'Snow White', 'Cinderella', 'Repunzel', 'Princess and the Frog', 'Beauty and the Beast', 'Brave', 'Merida', 'Sleeping Beauty', 'Frozen', 'Charming', 'Jasmine', 'Aladdin', 'Tangled'];
 
     let word = hangmanWords[Math.floor(Math.random() * hangmanWords.length)].toUpperCase();
     let answerArray = word.split('');
-		let guesses = this.state.guesses;
-		
-		let blankArray = [];
+    let guesses = this.state.guesses;
 
-		for (let i = 0; i < answerArray.length; i++) {
-			if(answerArray[i] === " ") {
-				blankArray[i] = " "
-			} else {
-				blankArray[i] = "_";
-			}
-		}
-		console.log('blank array', blankArray);
+    let blankArray = [];
 
-		let answerWord = blankArray.join("");
-		console.log('answer array', answerWord);
+    for (let i = 0; i < answerArray.length; i++) {
+      if (answerArray[i] === " ") {
+        blankArray[i] = " "
+      } else {
+        blankArray[i] = "_";
+      }
+    }
+    console.log('answer', word);
+
+    // let answerWord = blankArray.join("");
 
     this.setState({
       word,
-			answerArray,
-			answerWord: answerWord,
-			guesses,
-			guessedLetters: []
+      answerArray,
+      answerWord: blankArray,
+      guesses,
+      guessedLetters: []
 
     });
   }
@@ -158,24 +201,26 @@ export default class Game extends Component {
     return (
       <div className="game-container container">
         <h1>Hangman!</h1>
-        <button type="button" className="btn btn-success" onClick={this.chooseWord}>Start Game</button>
-				<div className = "row">
-          <div className="col-md-2" />
-					<div className="col-md-4">
-						<h2 className="stats">Wins: {this.state.wins}</h2>
-					</div>
-					<div className="col-md-4">
-						<h2 className="stats">Losses: {this.state.losses}</h2>
-					</div>
-          <div className="col-md-2" />
-				</div>
+        {!this.state.started ?
+          <button type="button" className="btn btn-success btn-lg" onClick={this.chooseWord}>Start Game</button> :
+          null
+        }
+        <div className="row user-stats">
+          <div className="col-sm-2" />
+          <div className="col-sm-4">
+            <h2 className="stats">Wins: {this.state.wins}</h2>
+          </div>
+          <div className="col-sm-4">
+            <h2 className="stats">Losses: {this.state.losses}</h2>
+          </div>
+        </div>
         <h2>Guesses Remaing: {this.state.guesses}</h2>
-        <h1>{this.state.word}</h1>
-        <h1>{this.state.answerArray}</h1>
-        <h1 className="answer-word">{this.state.answerWord}</h1>
+        <h1 className="answer-word">{this.state.answerWord.join('')}</h1>
+        {this.state.win ? <h2 className="win-lose">You Won!</h2> : null}
+        {this.state.lose ? <h2 className="win-lose">You Lost! Try Again!</h2> : null}
         <h2 className="guessed-letters">Guessed Letters: {this.state.guessedLetters.join(', ')}</h2>
-				{this.state.badKey ? <h2>Please select a letter</h2> : null}
-				{this.state.dupKey ? <h2>Please select a new letter</h2> : null}
+        {this.state.badKey ? <h2>Please select a letter</h2> : null}
+        {this.state.dupKey ? <h2>Please select a new letter</h2> : null}
 
       </div>
     )
